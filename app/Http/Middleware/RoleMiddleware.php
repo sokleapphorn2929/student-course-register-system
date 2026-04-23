1495a7eed0ab508715597c68ab0920f7381adefa
SOKLEAP <phorkzokl3ap@gmail.com>
2026-04-23 15:38:45 +0700

    api commit

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        if (!$request->user()?->role || !in_array($request->user()->role ,$roles)) {
+        return response()->json([
+                'message' => 'Your permission is rejected!'
+            ], 403);
+        }
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             "Admin" => \App\Http\Middleware\AdminMiddleware::class,
             "Teacher" => \App\Http\Middleware\TeacherMiddleware::class,
+            "role" => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
