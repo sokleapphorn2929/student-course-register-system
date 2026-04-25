a423df71fd18203dc08ce47dabd5b4a5b155b779
SOKLEAP <phorkzokl3ap@gmail.com>
2026-04-25 17:15:11 +0700

    deploy v2.7

--- routes/api.php
+++ routes/api.php
@@ -4,76 +4,74 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CourseAPIController;
 use App\Http\Controllers\Api\EnrollmentAPIController;
 use App\Http\Controllers\Api\StudentAPIController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TeacherAPIController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// Route::post("/register",[AuthController::class,"store"]);
+// Route::post("/login",[AuthController::class,"login"]);
+// Route::post("/logout",[AuthController::class,"logout"]);
 
-Route::post("/register",[AuthController::class,"store"]);
-Route::post("/login",[AuthController::class,"login"]);
-Route::post("/logout",[AuthController::class,"logout"]);
+// Route::middleware(["auth:sanctum","Admin"])->group(function(){
+//     Route::prefix("auth")->group(function(){
+//         Route::get("/",[AuthController::class,"index"]);
+//         Route::get("/me",[AuthController::class,"me"]);
+//         Route::get("/{id}",[AuthController::class,"show"]);
+//         Route::put("/{id}",[AuthController::class,"update"]);
+//         Route::delete("/{id}",[AuthController::class,"destroy"]);
+//     });
 
-Route::middleware(["auth:sanctum","Admin"])->group(function(){
-    Route::prefix("auth")->group(function(){
-        Route::get("/",[AuthController::class,"index"]);
-        Route::get("/me",[AuthController::class,"me"]);
-        Route::get("/{id}",[AuthController::class,"show"]);
-        Route::put("/{id}",[AuthController::class,"update"]);
-        Route::delete("/{id}",[AuthController::class,"destroy"]);
-    });
+//     Route::prefix("teachers")->group(function(){
+//         Route::get("/",[TeacherAPIController::class,"index"]);
+//         Route::post("/",[TeacherAPIController::class,"store"]);
+//         Route::get("/{id}",[TeacherAPIController::class,"show"]);
+//         Route::put("/{id}",[TeacherAPIController::class,"update"]);
+//         Route::delete("/{id}",[TeacherAPIController::class,"destroy"]);
+//     });
 
-    Route::prefix("teachers")->group(function(){
-        Route::get("/",[TeacherAPIController::class,"index"]);
-        Route::post("/",[TeacherAPIController::class,"store"]);
-        Route::get("/{id}",[TeacherAPIController::class,"show"]);
-        Route::put("/{id}",[TeacherAPIController::class,"update"]);
-        Route::delete("/{id}",[TeacherAPIController::class,"destroy"]);
-    });
+//     Route::prefix("course")->group(function(){
+//         Route::get("/",[CourseAPIController::class,"index"]);
+//         Route::post("/",[CourseAPIController::class,"store"]);
+//         Route::get("/{id}",[CourseAPIController::class,"show"]);
+//         Route::put("/{id}",[CourseAPIController::class,"update"]);
+//         Route::delete("/{id}",[CourseAPIController::class,"destroy"]);
+//     });
 
-    Route::prefix("course")->group(function(){
-        Route::get("/",[CourseAPIController::class,"index"]);
-        Route::post("/",[CourseAPIController::class,"store"]);
-        Route::get("/{id}",[CourseAPIController::class,"show"]);
-        Route::put("/{id}",[CourseAPIController::class,"update"]);
-        Route::delete("/{id}",[CourseAPIController::class,"destroy"]);
-    });
+//     Route::prefix("student")->group(function(){
+//         Route::get("/",[StudentAPIController::class,"index"]);
+//         Route::post("/",[StudentAPIController::class,"store"]);
+//         Route::get("/{id}",[StudentAPIController::class,"show"]);
+//         Route::put("/{id}",[StudentAPIController::class,"update"]);
+//         Route::delete("/{id}",[StudentAPIController::class,"destroy"]);
+//     });
 
-    Route::prefix("student")->group(function(){
-        Route::get("/",[StudentAPIController::class,"index"]);
-        Route::post("/",[StudentAPIController::class,"store"]);
-        Route::get("/{id}",[StudentAPIController::class,"show"]);
-        Route::put("/{id}",[StudentAPIController::class,"update"]);
-        Route::delete("/{id}",[StudentAPIController::class,"destroy"]);
-    });
+//     Route::prefix("enrollment")->group(function(){
+//         Route::get("/",[EnrollmentAPIController::class,"index"]);
+//         Route::post("/",[EnrollmentAPIController::class,"store"]);
+//         Route::get("/{id}",[EnrollmentAPIController::class,"show"]);
+//         Route::put("/{id}",[EnrollmentAPIController::class,"update"]);
+//         Route::delete("/{id}",[EnrollmentAPIController::class,"destroy"]);
+//     });
+// });
 
-    Route::prefix("enrollment")->group(function(){
-        Route::get("/",[EnrollmentAPIController::class,"index"]);
-        Route::post("/",[EnrollmentAPIController::class,"store"]);
-        Route::get("/{id}",[EnrollmentAPIController::class,"show"]);
-        Route::put("/{id}",[EnrollmentAPIController::class,"update"]);
-        Route::delete("/{id}",[EnrollmentAPIController::class,"destroy"]);
-    });
-});
+// Route::prefix("teachers")->middleware("auth:sanctum")->group(function(){
+//     Route::get("/",[TeacherAPIController::class,"index"]);
+//     Route::get("/{id}",[TeacherAPIController::class,"show"]);
+// });
 
-Route::prefix("teachers")->middleware("auth:sanctum")->group(function(){
-    Route::get("/",[TeacherAPIController::class,"index"]);
-    Route::get("/{id}",[TeacherAPIController::class,"show"]);
-});
-
-Route::prefix("course")->middleware("auth:sanctum")->group(function(){
-    Route::get("/",[CourseAPIController::class,"index"]);
-    Route::get("/{id}",[CourseAPIController::class,"show"]);
-});
+// Route::prefix("course")->middleware("auth:sanctum")->group(function(){
+//     Route::get("/",[CourseAPIController::class,"index"]);
+//     Route::get("/{id}",[CourseAPIController::class,"show"]);
+// });
     
-Route::prefix("student")->middleware("auth:sanctum")->group(function(){
-    Route::get("/",[StudentAPIController::class,"index"]);
-    Route::get("/{id}",[StudentAPIController::class,"show"]);
-});
+// Route::prefix("student")->middleware("auth:sanctum")->group(function(){
+//     Route::get("/",[StudentAPIController::class,"index"]);
+//     Route::get("/{id}",[StudentAPIController::class,"show"]);
+// });
 
-Route::prefix("enrollment")->middleware("auth:sanctum")->group(function(){
-    Route::get("/",[EnrollmentAPIController::class,"index"]);
-    Route::get("/{id}",[EnrollmentAPIController::class,"show"]);
-});
+// Route::prefix("enrollment")->middleware("auth:sanctum")->group(function(){
+//     Route::get("/",[EnrollmentAPIController::class,"index"]);
+//     Route::get("/{id}",[EnrollmentAPIController::class,"show"]);
+// });
+
+
+Route::get("/teachers",[TeacherAPIController::class,"index"]);
